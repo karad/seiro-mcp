5b0b743a529e54dc80eabf5fabdb10b4a4daf501
karad <kazuhiro+karad@gmail.com>
2025-12-19 17:11:59 +0900

    remove related speckit files

--- tests/integration.rs
+++ tests/integration.rs
@@ -10,8 +10,5 @@ mod runtime_spawn;
 #[path = "integration/auth_handshake.rs"]
 mod auth_handshake;
 
-#[path = "integration/refactor_contracts.rs"]
-mod refactor_contracts;
-
 #[path = "integration/refactor_behaviour.rs"]
 mod refactor_behaviour;
